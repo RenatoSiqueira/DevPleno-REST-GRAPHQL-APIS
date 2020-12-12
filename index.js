f99f8019885d6a40f9e72d3d4e61eef022f2d1b6
renatosiqueira <renatoelysiqueira@gmail.com>
2020-12-12 00:15:31 -0300

    Graphql Added

--- index.js
+++ index.js
@@ -4,7 +4,13 @@ const app = express();
 const bodyParser = require("body-parser");
 const routes = require("./routes");
 
+const graphqlServer = require("./graphql");
+
+// Rest
 app.use(bodyParser.json());
 app.use(routes);
+
+// GraphQl
+graphqlServer.applyMiddleware({ app });
 
 app.listen(3000, (err) => console.log("Server Running on port 3000"));
